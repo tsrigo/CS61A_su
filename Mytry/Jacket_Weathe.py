c48c576c94f7e68d89242f7839995f8537294dae
tsrigo <717660037@qq.com>
2022-06-27 16:12:04 +0800

    Add Jacket

---
+++ Mytry/Jacket_Weathe.py
@@ -0,0 +1,18 @@
+def wears_jacket_with_if(temp, raining):
+    """
+    >>> wears_jacket_with_if(90, False)
+    False
+    >>> wears_jacket_with_if(40, False)
+    True
+    >>> wears_jacket_with_if(100, True)
+    True
+    """
+    "*** YOUR CODE HERE ***"
+    if (temp <= 60 or raining) : 
+        return True
+    else :
+        return False
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()

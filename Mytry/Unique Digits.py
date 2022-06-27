15ce05bf7e89e790eca71d2df7cc6f8d9fb37777
tsrigo <717660037@qq.com>
2022-06-27 16:15:29 +0800

    Add unique_digits

---
+++ Mytry/Unique Digits.py
@@ -0,0 +1,39 @@
+from attr import has
+
+
+def unique_digits(n):
+    """Return the number of unique digits in positive integer n.
+
+    >>> unique_digits(8675309) # All are unique
+    7
+    >>> unique_digits(1313131) # 1 and 3
+    2
+    >>> unique_digits(13173131) # 1, 3, and 7
+    3
+    >>> unique_digits(10000) # 0 and 1
+    2
+    >>> unique_digits(101) # 0 and 1
+    2
+    >>> unique_digits(10) # 0 and 1
+    2
+    """
+    "*** YOUR CODE HERE ***"
+    ans = 0
+    for i in range(0, 10) :
+        ans += has_digit(n, i)
+    return ans
+
+def has_digit(n, k):
+    """Returns whether K is a digit in N.
+    >>> has_digit(10, 1)
+    True
+    >>> has_digit(12, 7)
+    False
+    """
+    "*** YOUR CODE HERE ***"
+    for i in str(n) :
+        if (i == str(k)) :
+            return True
+    return False
+
+unique_digits(100)

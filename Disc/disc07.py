eb4e8340f19dd1210c2c7506b3f7f07d7698cea2
tsrigo <717660037@qq.com>
2022-08-02 10:53:11 +0800

    Tree finished

--- Disc/disc07.py
+++ Disc/disc07.py
@@ -6,6 +6,23 @@ class Tree:
             assert isinstance(branch, Tree)
         self.branches = list(branches)
 
+    def __repr__(self):
+        if self.branches:
+            branch_str = ', ' + repr(self.branches)
+        else:
+            branch_str = ''
+        return 'Tree({0}{1})'.format(repr(self.label), branch_str)
+
+    def __str__(self):
+        return '\n'.join(self.indented())
+
+    def indented(self):
+        lines = []
+        for b in self.branches:
+            for line in b.indented():
+                lines.append('  ' + line)
+        return [str(self.label)] + lines
+
     def is_leaf(self):
         return not self.branches
      
@@ -56,7 +73,7 @@ def max_path_sum(t):
             tep = hh
     return tep + t.label
     # subsitute ans
-    return t.label + max([max_path_sum(b) for b in t.branches])
+    # return t.label + max([max_path_sum(b) for b in t.branches])
 
 def find_path(t, x):
     """
@@ -72,4 +89,28 @@ def find_path(t, x):
         tep = find_path(i, x)
         if tep:
             return [t.label] + tep
+
+def prune_small(t, n):
+    """Prune the tree mutatively, keeping only the n branches
+    of each node with the smallest label.
+
+    >>> t1 = Tree(6)
+    >>> prune_small(t1, 2)
+    >>> t1
+    Tree(6)
+    >>> t2 = Tree(6, [Tree(3), Tree(4)])
+    >>> prune_small(t2, 1)
+    >>> t2
+    Tree(6, [Tree(3)])
+    >>> t3 = Tree(6, [Tree(1), Tree(3, [Tree(1), Tree(2), Tree(3)]), Tree(5, [Tree(3), Tree(4)])])
+    >>> prune_small(t3, 2)
+    >>> t3
+    Tree(6, [Tree(1), Tree(3, [Tree(1), Tree(2)])])
+    """
+    while len(t.branches) > n:
+        largest = max([x for x in t.branches], key=lambda t: t.label)
+        t.branches = [x for x in t.branches if x.label != largest.label]
+        # t.branches.remove(largest)
+    for sub in t.branches:
+        prune_small(sub, n)
 

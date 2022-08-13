b3b89ad8725792b88f28a28b13cd70533161bb69
tsrigo <717660037@qq.com>
2022-08-13 12:06:17 +0800

    Finished lab08

--- Lab/lab08/lab08.scm
+++ Lab/lab08/lab08.scm
@@ -1,9 +1,44 @@
-(define (over-or-under num1 num2) 'YOUR-CODE-HERE)
+(define (over-or-under num1 num2) 
+    '(if (= num1 num2) 0 (if (< num1 num2) -1 (if (> num1 num2) 1) ))
+    (cond
+        ((= num1 num2) 0)
+        ((< num1 num2) -1)
+        (else 1)
+    )
+)
 
-(define (composed f g) 'YOUR-CODE-HERE)
+(define (composed f g)
+    (define (help x)
+        (f (g x))
+    )
+    (lambda (x) (help x))
+)
 
 (define (square n) (* n n))
 
-(define (pow base exp) 'YOUR-CODE-HERE)
+(define (pow base exp) 
+    (cond
+        ((= 1 base) 1)
+        ((= 0 exp) 1)
+        ((= 1 exp) base)
+        (else 
+            (if (even? exp)
+                (pow (square base) (/ exp 2))
+                (* base (pow base (- exp 1)))
+            )
+        )
+    )
+)
 
-(define (ascending? lst) 'YOUR-CODE-HERE)
+(define (ascending? lst) 
+    (if (or (null? lst) (null? (cdr lst)))
+        #t
+        (and
+            (or
+                (< (car lst) (car (cdr lst)))
+                (= (car lst) (car (cdr lst)))
+            )
+            (ascending? (cdr lst))
+        )
+    )
+)

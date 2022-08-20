d3c52a5b10f1f3b38f8f91dda5617b857625bfab
tsrigo <717660037@qq.com>
2022-08-20 22:12:00 +0800

    Part1 Finished

--- Proj/Proj4-scheme/scheme_eval_apply.py
+++ Proj/Proj4-scheme/scheme_eval_apply.py
@@ -5,6 +5,7 @@ from scheme_utils import *
 from ucb import main, trace
 
 import scheme_forms
+import scheme_builtins
 
 ##############
 # Eval/Apply #
@@ -35,6 +36,9 @@ def scheme_eval(expr, env, _=None):  # Optional third argument is ignored
     else:
         # BEGIN PROBLEM 3
         "*** YOUR CODE HERE ***"
+        procedure = scheme_eval(first, env)
+        args = rest.map(lambda x: scheme_eval(x, env))
+        return scheme_apply(procedure, args, env)
         # END PROBLEM 3
 
 

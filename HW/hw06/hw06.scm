eb331b76087074c9fb1c057f933a7a7c20019260
tsrigo <717660037@qq.com>
2022-08-21 14:13:40 +0800

    Q2 finished

--- HW/hw06/hw06.scm
+++ HW/hw06/hw06.scm
@@ -8,7 +8,30 @@
     (car (cddr s))
 )
 
-(define (interleave lst1 lst2) 'YOUR-CODE-HERE)
+(define (interleave lst1 lst2) 
+    (define (help lst1 lst2 ans choose)
+        (cond 
+            ((null? lst1) 
+                (define ans (append ans lst2))
+                ans            
+            )
+            ((null? lst2) 
+                (define ans (append ans lst1))
+                ans            
+            )
+
+            ((= 1 choose)
+                (define ans (append ans (list (car lst1))))
+                (help (cdr lst1 ) lst2 ans 2)
+            )
+            ((= 2 choose)
+                (define ans (append ans (list (car lst2))))
+                (help lst1 (cdr lst2) ans 1)
+            )
+        )
+    )
+    (help lst1 lst2 nil 1)
+)
 
 (define (my-filter pred lst) 'YOUR-CODE-HERE)
 

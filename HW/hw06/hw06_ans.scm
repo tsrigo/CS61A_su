ccdfc372786c859becaf7ae78f98161ea628b701
tsrigo <717660037@qq.com>
2022-08-21 14:52:19 +0800

    Fabulous answer!!!

---
+++ HW/hw06/hw06_ans.scm
@@ -0,0 +1,13 @@
+(define (interleave lst1 lst2)
+  (if (or (null? lst1) (null? lst2))
+      (append lst1 lst2)
+      (cons (car lst1)
+            (cons (car lst2)
+                  (interleave (cdr lst1) (cdr lst2)))))
+)   
+
+(define (my-filter pred lst)
+  (cond ((null? lst) '())
+        ((pred (car lst)) (cons (car lst) (my-filter pred (cdr lst))))
+        (else (my-filter pred (cdr lst))))
+)

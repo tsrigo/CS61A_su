97ab24cf55e5016b66932353c43dbf028b3ce4fc
tsrigo <717660037@qq.com>
2022-08-22 17:02:30 +0800

    Problem 16 Finished

--- Proj/Proj4-scheme/questions.scm
+++ Proj/Proj4-scheme/questions.scm
@@ -20,8 +20,12 @@
 ;; Merge two lists LIST1 and LIST2 according to ORDERED? and return
 ;; the merged lists.
 (define (merge ordered? list1 list2)
-  ; BEGIN PROBLEM 16
-  'replace-this-line
+  (if (or (null? list1) (null? list2)) 
+    (append list1 list2)
+    (if (ordered? (car list1) (car list2))
+      (cons (car list1) (merge ordered? (cdr list1) list2))
+      (cons (car list2) (merge ordered? list1 (cdr list2)))
+    )
   )
-  ; END PROBLEM 16
+)
 
